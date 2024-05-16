33e5c1492b3296741d6ea8d6ce9ffd4c1e459127
JuanJose0927 <juanjosearbelaez689@gmail.com>
2024-05-16 14:20:20 -0500

    advances in a proyect

--- src/app/pages/destinations/destinations.component.ts
+++ src/app/pages/destinations/destinations.component.ts
@@ -99,44 +99,134 @@ export class DestinationsComponent {
     {
       nombre: "Japan",
       curency: "JPY",
-      image: ""
+      image: "https://cdn.britannica.com/63/59363-050-F082087B/Garden-Kinkaku-Temple-shelter-structure-use-landscape.jpg"
     },
     {
       nombre: "North Korea",
       curency: "KPW",
-      image: ""
+      image: "https://i.natgeofe.com/k/8c2c618a-3daa-40dc-9ee4-32f15b811e2f/City_N-Korea_KIDS_0521_16x9.jpg"
     },
     {
       nombre: "Qatar",
       curency: "QAR",
-      image: ""
+      image: "https://wyandottedaily.com/wp-content/uploads/2024/01/Qatar.jpg"
     },
     {
       nombre: "Phillippines",
       curency: "PHP",
-      image: ""
+      image: "https://i.natgeofe.com/n/54720271-2656-4eae-a088-a2023cc25c9f/historic-colonial-town-of-vigan_3x2.jpg"
     },
     {
       nombre: "Saudi Arabia",
       curency: "SAR",
-      image: ""
+      image: "https://cdn.britannica.com/11/189711-050-2EFCF158/view-Mecca-Saudi-Arabia.jpg"
     },
     {
       nombre: "South Korea",
       curency: "KRW",
-      image: ""
+      image: "https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F15ebd41e-09ac-41db-be13-6dbead2c03e6.jpg?crop=1564%2C880%2C318%2C0&resize=1200"
     },
     {
       nombre: "United Arab Emirates",
       curency: "AED",
-      image: ""
+      image: "https://cdn.britannica.com/15/189715-050-4310222B/Dubai-United-Arab-Emirates-Burj-Khalifa-top.jpg "
     },
+    {
+      nombre: "Thailand",
+      curency: "THB",
+      image: "https://www.intrepidtravel.com/v3/assets/blt0de87ff52d9c34a8/bltf2f8a1a85ab37903/63c8b28ec82c4c38f2dbeef4/thailand_sukhothai_historical-park-1100.jpg"
+    },
+    {
+      nombre: "Uzbekistan",
+      curency: "UZS",
+      image: "https://idsb.tmgrup.com.tr/ly/uploads/images/2021/10/25/154544.jpg"
+    },
+    {
+      nombre: "India",
+      curency: "INR",
+      image: "https://humanidades.com/wp-content/uploads/2018/12/India-1-e1585355432124.jpg"
+    },
+    {
+      nombre: "China",
+      curency: "CNY",
+      image: "https://www.intrepidtravel.com/adventures/wp-content/uploads/2017/08/china_shanghai_yuyuan-garden-city.jpg"
+    }
   ]
   Africa = [
     {
-      nombre: "",
-      curency: "",
-      image: ""
-    }
+      nombre: "Algeria",
+      curency: "DZD",
+      image: "https://cdn.britannica.com/00/77300-050-F7C43044/View-city-Algiers-Algeria.jpg"
+    },
+    {
+      nombre: "Angola",
+      curency: "AOA",
+      image: "https://mediaim.expedia.com/destination/1/812169e9f9280fb407e27725a022e4f5.jpg"
+    },
+    {
+      nombre: "Cameroon",
+      curency: "XAF",
+      image: "https://africanmediaagency.com/wp-content/uploads/2023/06/image-27.png"
+    },
+    {
+      nombre: "Central African Republic",
+      curency: "XAF",
+      image: "https://untamedborders.com/wp-content/uploads/2022/04/IMG_5713.jpg"
+    },
+    {
+      nombre: "Republic of the Congo",
+      curency: "XAF",
+      image: "https://pic-roc.com/wp-content/uploads/2023/10/Explorer-la-Republique-du-Congo-blog-hero.jpg"
+    },
+    {
+      nombre: "Egypt",
+      curency: "EGP",
+      image: "https://www.rjtravelagency.com/wp-content/uploads/2023/07/Cairo-Egypt.jpg"
+    },
+    {
+      nombre: "Ghana",
+      curency: "GHS",
+      image: "https://mediaim.expedia.com/destination/1/be0ad25550296ffd601bb7d3dd6e3807.jpg"
+    },
+    {
+      nombre: "Kenya",
+      curency: "KES",
+      image: "https://commonwealthchamber.com/wp-content/uploads/2021/07/kenya-1.jpg"
+    },
+    {
+      nombre: "Madagascar",
+      curency: "MGA",
+      image: "https://static.wixstatic.com/media/7b021c_d33e629d12fc4ba1a1ed3ccc8a0de840~mv2.jpg/v1/fill/w_1000,h_664,al_c,q_85,enc_auto/7b021c_d33e629d12fc4ba1a1ed3ccc8a0de840~mv2.jpg"
+    },
+    {
+      nombre: "Morocco",
+      curency: "MAD",
+      image: "https://media.nomadicmatt.com/moroccoguide.jpg"
+    },
+    {
+      nombre: "Mozambique",
+      curency: "MZN",
+      image: "https://cdn.britannica.com/07/93107-050-A0D6E501/Maputo-Mozambique.jpg"
+    },
+    {
+      nombre: "Nigeria",
+      curency: "MZN",
+      image: "https://images.adsttc.com/media/images/5f35/9efe/b357/657e/fc00/0072/large_jpg/Lagos_Nigeria.jpg?1597349616"
+    },
+    {
+      nombre: "Senegal",
+      curency: "XOF",
+      image: "https://lp-cms-production.imgix.net/2023-03/GettyImages-694213668.jpg?sharp=10&vib=20&w=1200&w=600&h=400"
+    },
+    {
+      nombre: "South Africa",
+      curency: " ZAR",
+      image: "https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F4b50e023-2a4b-4f99-8cb4-6d383858a385.jpg?crop=1564%2C880%2C318%2C0&resize=1200"
+    },
+    {
+      nombre: "Tanzania",
+      curency: " TZS",
+      image: "https://viajesportanzania.com/wp-content/uploads/viaje-a-tanzania-medida-taranna-005-680x383.jpg"
+    },
   ]
 }
